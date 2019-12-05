bfa433cc89bd43c9f73253a6838be3048ce9f34a
valeria134 <valeriagbi.pires@gmail.com>
2019-12-05 13:16:41 -0300

    Commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from form import CadastroForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user
+ 
+app = Flask (__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'secret'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(80), unique=True, nullable=True)
+    email = db.Column(db.String(12), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.usuario
+
+    
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/cadastro', methods=["GET", "POST"])
+def Cadastro():
+    form = CadastroForm()
+    if form.validate_on_submit():
+        user = User()
+        usuario = request.form['usuario']
+        email = request.form['email']
+        password = request.form['password']
+        db.session(user)
+        db.session.commit()
+        
+    return render_template(
+        'cadastro.html',
+        form=form
+        )
+
+
+@app.route('/login', methods=["GET", "POST"])
+def Login():
+    form=LoginForm()
+    return render_template(
+        'login.html',
+         form=form
+         )    
+@app.route('/materiais', methods=["GET", "POST"])
+def Materiais():
+    return render_template('materiais.html')
+@app.route('/assunto', methods=["GET", "POST"])
+def Assunto():
+    return render_template('assunto.html')
+          
+
+if (__name__ =='__main__'):
+    app.run(debug=True, port = 5001)
---
+++ form.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class CadastroForm(FlaskForm):
+    usuario = StringField("Usuario", validators= [DataRequired()])
+    email = StringField("Email", validators= [DataRequired(),Email()])
+    password = PasswordField("Senha",[DataRequired(),EqualTo('confirm',message='Senhas diferentes')])
+    submit = SubmitField('Cadastrar')
+
+
+class LoginForm(FlaskForm):
+    email= StringField("Email", validators= [DataRequired(),Email()])
+    senha= PasswordField("Senha",[DataRequired(),EqualTo('confirm',message='Senhas diferentes')])
+    remember_me= BooleanField('Lembrar_senha')
+    submit = SubmitField('Login')
+
